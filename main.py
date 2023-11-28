d28cafa6a0052a6dbae38cf80e01d68faeadd33d
TetraspaceW <holomanga@gmail.com>
2023-11-28 10:38:38 +0000

    remove unneeded client import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from geopy.geocoders import Nominatim
-from supabase import create_client, Client
+from supabase import create_client
 
 intents = discord.Intents.default()
 intents.message_content = True
