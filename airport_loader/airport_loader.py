3284d0d28ba4b64d5920aa3aa5d0803512b0b8b3
TetraspaceW <holomanga@gmail.com>
2023-12-19 23:14:01 +0000

    Format

--- airport_loader/airport_loader.py
+++ airport_loader/airport_loader.py
@@ -4,41 +4,53 @@ import csv
 import os
 from supabase import create_client
 
-supabase_url = os.environ.get('SUPABASE_URL')
-supabase_key = os.environ.get('SUPABASE_KEY')
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(supabase_url, supabase_key)
 
 
 def update_airport_names(airports: list[str] = None):
-    with open('./airport_loader/frontier_airports.csv') as file:
-        airports_reader = csv.reader(file, quotechar='\'', delimiter=',')
+    with open("./airport_loader/frontier_airports.csv") as file:
+        airports_reader = csv.reader(file, quotechar="'", delimiter=",")
         for row in airports_reader:
             [code, name] = row
             if (not airports) or (code in airports):
-                airports_table = supabase.table('airport')
-                airports_table.update({'name': name}).eq('code', code).execute()
+                airports_table = supabase.table("airport")
+                airports_table.update({"name": name}).eq("code", code).execute()
                 print(f"Updated name of {code} to {name}.")
 
 
 # TODO: populate OR update depending on whether they already exist
 def populate_geocoded_airports(airports: list[str]):
-    with open('./airport_loader/frontier_airports.csv') as file:
-        airports_reader = csv.reader(file, quotechar='\'', delimiter=',')
+    with open("./airport_loader/frontier_airports.csv") as file:
+        airports_reader = csv.reader(file, quotechar="'", delimiter=",")
         for row in airports_reader:
             [code, name] = row
             if (not airports) or code in airports:
-                airports_table = supabase.table('airport')
-                airport_location: geopy.Location = geopy.Nominatim(user_agent='ampmap-airport-loader').geocode(
-                    f"{code} airport", exactly_one=True)
+                airports_table = supabase.table("airport")
+                airport_location: geopy.Location = geopy.Nominatim(
+                    user_agent="ampmap-airport-loader"
+                ).geocode(f"{code} airport", exactly_one=True)
                 print(airport_location)
                 airports_table.insert(
-                    {"location": {"lat": airport_location.latitude, "lng": airport_location.longitude}, "code": code,
-                     "frontier": True, "name": name}).execute()
+                    {
+                        "location": {
+                            "lat": airport_location.latitude,
+                            "lng": airport_location.longitude,
+                        },
+                        "code": code,
+                        "frontier": True,
+                        "name": name,
+                    }
+                ).execute()
                 time.sleep(2)  # let's not get rate limited
 
 
 # TODO: implement this function
 def populate_routes():
-    routes_table = supabase.table('route')
-    # TODO: probably we want more data for routes
-    routes_table.insert({"start": None, "end": None, "frontier": None}).execute()
+    routes_table = supabase.table("route")
+    # TODO: add some web scraping from the frontier website
+
+    routes_table.insert(
+        {"start": None, "end": None, "frontier": None, "length": None}
+    ).execute()
--- main.py
+++ main.py
@@ -12,8 +12,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix="!ampmap ", intents=intents)
 
-supabase_url = os.environ.get('SUPABASE_URL')
-supabase_key = os.environ.get('SUPABASE_KEY')
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(supabase_url, supabase_key)
 
 
@@ -30,36 +30,47 @@ async def on_command_error(ctx: commands.Context, _):
 @client.command()
 async def location(ctx: commands.Context, *args):
     location_input = " ".join(args)
-    geolocator = Nominatim(user_agent='ampmap')
-    geocoded_location: geopy.Location = geolocator.geocode(location_input, exactly_one=True)
-    coordinates: Location = {'lat': geocoded_location.latitude, 'lng': geocoded_location.longitude}
+    geolocator = Nominatim(user_agent="ampmap")
+    geocoded_location: geopy.Location = geolocator.geocode(
+        location_input, exactly_one=True
+    )
+    coordinates: Location = {
+        "lat": geocoded_location.latitude,
+        "lng": geocoded_location.longitude,
+    }
 
-    locations_table = supabase.table('location')
+    locations_table = supabase.table("location")
 
-    _, id_exists = locations_table.select('user_id').eq('user_id', ctx.author.id).execute()
+    _, id_exists = (
+        locations_table.select("user_id").eq("user_id", ctx.author.id).execute()
+    )
 
     airport_data = nearest_airport(coordinates)
-    if airport_data['distance'] <= 100:
-        airport_code = airport_data['code']
+    if airport_data["distance"] <= 100:
+        airport_code = airport_data["code"]
     else:
         airport_code = None
 
-    supabase_request = {'location': coordinates, 'user_name': ctx.author.name, 'nearest_airport': airport_code}
+    supabase_request = {
+        "location": coordinates,
+        "user_name": ctx.author.name,
+        "nearest_airport": airport_code,
+    }
 
     if id_exists:
-        locations_table.update(supabase_request).eq('user_id', ctx.author.id).execute()
+        locations_table.update(supabase_request).eq("user_id", ctx.author.id).execute()
     else:
         supabase_request["user_id"] = ctx.author.id
         locations_table.insert(supabase_request).execute()
 
     await ctx.send(f"Your nearest airport is {nearest_airport(coordinates)}")
-    
+
 
 @client.command()
 async def obscure(ctx: commands.Context):
     locations_table = supabase.table("location")
-    locations_table.delete().eq('user_id', ctx.author.id).execute()
-    await ctx.send(f"Obscured your location from amp's sight.")
+    locations_table.delete().eq("user_id", ctx.author.id).execute()
+    await ctx.send(f"Removed your location from the database.")
 
 
 class Location(TypedDict):
@@ -68,13 +79,24 @@ class Location(TypedDict):
 
 
 def nearest_airport(location: Location, frontier: bool = True):
-    airports = supabase.table('airport').select('code,location,name').eq('frontier', frontier).execute()
-    distances = [{'name': airport['name'], 'code': airport['code'],
-                  'distance': geodesic((location['lat'], location['lng']),
-                                       (airport['location']['lat'], airport['location']['lng'])).miles}
-                 for airport
-                 in airports.data]
-    distances.sort(key=lambda d: d['distance'])
+    airports = (
+        supabase.table("airport")
+        .select("code,location,name")
+        .eq("frontier", frontier)
+        .execute()
+    )
+    distances = [
+        {
+            "name": airport["name"],
+            "code": airport["code"],
+            "distance": geodesic(
+                (location["lat"], location["lng"]),
+                (airport["location"]["lat"], airport["location"]["lng"]),
+            ).miles,
+        }
+        for airport in airports.data
+    ]
+    distances.sort(key=lambda d: d["distance"])
     return distances[0]
 
 
